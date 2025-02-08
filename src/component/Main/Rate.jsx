d13db9ea031ad5edd67967e8632162463bcd0ee3
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-02-08 14:11:02 +0430

    styling RatePage

--- src/component/Main/Rate.jsx
+++ src/component/Main/Rate.jsx
@@ -1,7 +1,30 @@
+import styles from './Rate.module.css'
 function Rate() {
+    // const date=new Date();
     return (
-        <div>
-            Rate of money 
+        <div className={styles.container}>
+           <form action="" className={styles.formContainer}>
+              <div className={styles.labelPart1}>
+                <label>افغانی</label>
+                <label>کلدار</label>
+                <label>تومن</label>
+              </div>
+              <div className={styles.inputPart1}>
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              </div>
+              <div className={styles.labelPart2}>
+                <label>دالر</label>
+                <label>دالر</label>
+                <label>دالر</label>
+              </div>
+              <div className={styles.inputPart2}>
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+              </div>
+           </form>
         </div>
     )
 }
