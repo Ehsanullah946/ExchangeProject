ab7b1989d8d4c5f5ef8e35ee9bb6345c9320cf62
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-03-25 10:53:01 +0430

    some styles

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,27 +1,63 @@
-import {NavLink } from "react-router-dom"
-import styles from './Header.module.css'
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
 function Header() {
-    return (
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-                <NavLink className={styles.active} activeClassName={styles.active} to="/main">Main Page</NavLink>
-            </li>
-            <li>
-                <NavLink  to="/Bank">Bank</NavLink>
-            </li>
-            <li>
-                <NavLink to="/management">Management</NavLink>
-            </li>
-            <li>
-                <NavLink to="/reports">Reports</NavLink>
-            </li>              
-            <li>
-                <NavLink to="/setting">Settings</NavLink>
-            </li>              
-          </ul>
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink
+            to="/main"
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : ""
+            }
+          >
+            Main Page
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/Bank"
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : ""
+            }
+          >
+            Bank
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/management"
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : ""
+            }
+          >
+            Management
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/reports"
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : ""
+            }
+          >
+            Reports
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/setting"
+            className={({ isActive }) =>
+              isActive ? `${styles.active}` : ""
+            }
+          >
+            Settings
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
